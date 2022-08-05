62d9d51088451511e9fae66d426813fc97750c5d
Huy Tran <tmhuyy.itiu@gmail.com>
2022-08-05 09:34:31 +0700

    config mongoose in entry file

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
-require('dotenv').config();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+mongoose
+  .connect("mongodb://localhost:27017/vn-camp", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("CONNECTION MONGODB"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
@@ -8,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // test
 app.get("/", (req, res) => {
-    res.send("TEST")
+  res.send("TEST");
 });
 
-app.listen(process.env.PORT || "5000", () => console.log("SERVER IS RUNNING"))
+app.listen(process.env.PORT || "5000", () => console.log("SERVER IS RUNNING"));
